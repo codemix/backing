6e04ba9279376f1e052842e78dda82af717a1069
Charles Pick <charles.pick@gmail.com>
2016-02-18 00:02:39 +0000

    sniff features for polyfill

--- test-polyfill.js
+++ test-polyfill.js
@@ -1,1 +1,17 @@
-require("babel-register")();
+try {
+  (new Function('var a = (args) => true; var b = []; b.push(...b);'))();
+  console.log('Using modern ES environment.');
+  require("babel-register")();
+}
+catch (e) {
+  console.log('Using legacy ES environment.', e.stack);
+  // Legacy environment.
+  require("babel-register")({
+    "presets": ["es2015", "stage-0", "react"],
+    "plugins": [
+      "transform-runtime"
+    ],
+    "passPerPreset": true
+  });
+}
+
